93f6b961588843570ad5d0bf90aaf735d1eebc86
DELIDJI AUREL CELYN GBAGUIDI <aurelgbaguidi@gmail.com>
2024-02-05 02:57:57 +0100

    Add main menu

--- BMS.cpp
+++ BMS.cpp
@@ -25,24 +25,67 @@ public:
 
 void BankAccount::createAccount(){
 
-    system("cls");
-    cout << "Enter the account number : ";
+    cout << "                                                                 \tEnter the account number : ";
     cin >> accountNumber;
-    cout << "Enter the name of the account holder : ";
+    cout << "                                                                 \tEnter the name of the account holder : ";
     cin.ignore();
     cin.getline(name, 50);
-    cout << "Enter the type of account (C/S) : ";
+    cout << "                                                                 \tEnter the type of account (C/S) : ";
     cin >> type;
     type = toupper(type);
-    cout << "Enter the initial amount( >= 500 for saving and >= 1000 for current) :  ";
+    cout << "                                                                 \tEnter the initial amount( >= 500 for saving and >= 1000 for current) : ";
     cin >> moneyDeposit;
-    cout << "Account created successfully !" << endl;
+    cout << "                                                                    \tAccount created successfully !" << endl;
+    cout << endl;
+
+    cout << "\t -----------------------------------------------------------------------------------------------------------------------------------------------------" << endl;
+
+    cout << endl;
 
 }
 
 void BankAccount::showAccount() const{
-    cout << "Account Number : " << accountNumber << endl;
-    cout << "Account Holder Name : " << name << endl;
-    cout << "Type of Account : " << type << endl;
-    cout << "Balance amount : " << moneyDeposit << endl;
+    cout << "                                                                \t Account Number : " << accountNumber << endl;
+    cout << "                                                                \t Account Holder Name : " << name << endl;
+    cout << "                                                                \t Type of Account : " << type << endl;
+    cout << "                                                                \t Balance amount : " << moneyDeposit << endl;
+}
+
+int main(){
+
+    char ch;
+    int num;
+
+
+    system("cls");
+
+    cout << "###################################################################################################################################################################" << endl;
+    
+    cout << "#                                                               Welcome to the Bank Management System                                                             #" << endl;
+    
+    cout << "###################################################################################################################################################################" << endl;
+    
+    cout << endl;
+
+    cout << "\t ----------------------------------------------------------------------Main Menu----------------------------------------------------------------------" << endl;
+    cout << endl;
+
+    cout << "                                                                \t 1. Create a new account                                                                   " << endl;
+    cout << "                                                                \t 2. Deposit money                                                                " << endl;
+    cout << "                                                                \t 3. Withdraw money                                                                " << endl;
+    cout << "                                                                \t 4. Balance enquiry                                                                " << endl;
+    cout << "                                                                \t 5. All account holder list                                                                " << endl;
+    cout << "                                                                \t 6. Close an account                                                                " << endl;
+    cout << "                                                                \t 7. Modify an account                                                                " << endl;
+    cout << "                                                                \t 8. Exit                                                                " << endl;
+    cout << endl;
+
+    cout << "\t -----------------------------------------------------------------------------------------------------------------------------------------------------" << endl;
+    cout << endl;
+
+    BankAccount zen;
+    zen.createAccount();
+    zen.showAccount();
+
+    return 0;
 }
